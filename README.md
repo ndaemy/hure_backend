3178e00021fd111a69b09e5f53d2e4334246d606
yuyaebean <yuyaebean@gmail.com>
2020-01-24 01:11:50 +0900

    Create GraphQL Server template

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # HYRE_backend
-한양대학교 부동산융합대학원 원우회 앱 backend / Hanyang Univ. Real Estate Graduate School reunion app backend
+
+Hanyang Univ. Real Estate Graduate School reunion app backend  
+한양대학교 부동산융합대학원 원우회 앱 backend
+
+## Environment variables
+
+- PORT (optional): port number of GraphQL Server / Playground
+
+### Example .env file
+
+```
+PORT=4000
+```
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('working!');
+import { GraphQLServer } from 'graphql-yoga';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hi'
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: PORT }, () =>
+  console.log(`Server is now running on http://localhost:${PORT}`)
+);

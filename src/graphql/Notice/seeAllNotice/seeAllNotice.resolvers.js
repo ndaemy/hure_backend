a18327c8a87f3d3b2abd53da3b0a4ea998bba154
yuyaebean <yuyaebean@gmail.com>
2020-02-28 21:25:38 +0900

    Modify seeAllNotice resolver

--- src/graphql/Notice/seeAllNotice/seeAllNotice.resolvers.js
+++ src/graphql/Notice/seeAllNotice/seeAllNotice.resolvers.js
@@ -2,9 +2,13 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Query: {
-    seeAllNotice: async (_, __, { request, isAdminOrUser }) => {
+    seeAllNotice: async (_, { limit, page }, { request, isAdminOrUser }) => {
       isAdminOrUser(request);
-      return await prisma.notices({ orderBy: 'createdAt_DESC' });
+      return await prisma.notices({
+        orderBy: 'createdAt_DESC',
+        skip: limit * (page - 1),
+        first: limit
+      });
     }
   }
 };

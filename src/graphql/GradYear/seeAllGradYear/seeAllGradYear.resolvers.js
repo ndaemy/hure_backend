8ad2b03d9f5b46ff30efa17947cb4869df7bcb77
yuyaebean <yuyaebean@gmail.com>
2020-03-02 18:20:17 +0900

    seeAllGradYear signin option off

--- src/graphql/GradYear/seeAllGradYear/seeAllGradYear.resolvers.js
+++ src/graphql/GradYear/seeAllGradYear/seeAllGradYear.resolvers.js
@@ -2,9 +2,7 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Query: {
-    seeAllGradYear: async (_, __, { request, isAdminOrUser }) => {
-      isAdminOrUser(request);
-      return await prisma.gradYears({ orderBy: 'generation_DESC' });
-    }
+    seeAllGradYear: async () =>
+      await prisma.gradYears({ orderBy: 'generation_DESC' })
   }
 };

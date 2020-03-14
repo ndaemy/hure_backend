eba86e0af21d0c1863d108f60b83f97d2aa639b8
yuyaebean <yuyaebean@gmail.com>
2020-03-14 12:07:09 +0900

    Add computed fields to type Major

--- src/graphql/Major/Major.resolvers.js
+++ src/graphql/Major/Major.resolvers.js
@@ -2,6 +2,21 @@ import { prisma } from '../../../generated/prisma-client';
 
 export default {
   Major: {
-    users: async ({ id }) => await prisma.major({ id }).users()
+    users: async ({ id }) => await prisma.major({ id }).users(),
+    profs: async ({ id }) => await prisma.major({ id }).profs(),
+    howManyUsers: async ({ id }) =>
+      await prisma
+        .usersConnection({
+          where: { AND: [{ isConfirmed: true }, { major: { id } }] }
+        })
+        .aggregate()
+        .count(),
+    howManyProfs: async ({ id }) =>
+      await prisma
+        .profsConnection({
+          where: { major: { id } }
+        })
+        .aggregate()
+        .count()
   }
 };

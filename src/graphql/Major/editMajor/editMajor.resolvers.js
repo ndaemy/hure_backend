7c81c6b240427606b4aa45852a6812608269021b
yuyaebean <yuyaebean@gmail.com>
2020-04-08 17:41:29 +0900

    Edit editMajor resolver #2

--- src/graphql/Major/editMajor/editMajor.resolvers.js
+++ src/graphql/Major/editMajor/editMajor.resolvers.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Mutation: {
-    editMajor: async (_, { id, name }, { request, isAdmin }) => {
+    editMajor: async (_, { id, name, shortName }, { request, isAdmin }) => {
       isAdmin(request);
       return await prisma.updateMajor({
         where: { id },

af528007ab77a3bdb73359ada89296e7576597b3
yuyaebean <yuyaebean@gmail.com>
2020-04-22 16:12:38 +0900

    Edit seeAllExecutive resolver
    Reorder by title

--- src/graphql/Executive/seeAllExecutive/seeAllExecutive.resolvers.js
+++ src/graphql/Executive/seeAllExecutive/seeAllExecutive.resolvers.js
@@ -1,12 +1,23 @@
 import { prisma } from '../../../../generated/prisma-client';
 
+const compareByTitle = (a, b) => {
+  if (a.generation < b.generation) return -1;
+  else if (a.generation > b.generation) return 1;
+  else {
+    if (a.title > b.title) return -1;
+    else if (a.title < b.title) return 1;
+    else return 0;
+  }
+};
+
 export default {
   Query: {
     seeAllExecutive: async (_, __, { request, isAdminOrUser }) => {
       isAdminOrUser(request);
-      return await prisma.executives({
-        orderBy: 'generation_ASC'
+      const originalArray = await prisma.executives({
+        orderBy: 'generation_ASC',
       });
-    }
-  }
+      return originalArray.sort(compareByTitle);
+    },
+  },
 };

4dc462d7d419e67a3be5720f4da7f1b7317ead08
yuyaebean <yuyaebean@gmail.com>
2020-04-24 21:36:53 +0900

    Edit seeAllUser resolver

--- src/graphql/User/seeAllUser/seeAllUser.resolvers.js
+++ src/graphql/User/seeAllUser/seeAllUser.resolvers.js
@@ -1,9 +1,37 @@
 import { prisma } from '../../../../generated/prisma-client';
 
-const compareByName = (a, b) => {
-  if (a.name > b.name) return 1;
-  if (a.name < b.name) return -1;
-  if (a.name === b.name) return 0;
+const FRAGMENT = `
+  fragment Userparts on User {
+    id
+    name
+    birthday
+    email
+    cellPhone
+    company
+    companyDesc
+    team
+    position
+    workPhone
+    workAddress
+    photo
+    isConfirmed
+    major {
+      name
+    }
+    graduatedYear {
+      generation
+    }
+  }
+`;
+
+const compareByGenName = (a, b) => {
+  if (a.graduatedYear.generation > b.graduatedYear.generation) return 1;
+  if (a.graduatedYear.generation < b.graduatedYear.generation) return -1;
+  if (a.graduatedYear.generation === b.graduatedYear.generation) {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+    if (a.name === b.name) return 0;
+  }
 };
 
 export default {
@@ -12,19 +40,21 @@ export default {
       isAdminOrUser(request);
       const { limit, page, major, generation } = args;
 
-      const users = await prisma.users({
-        orderBy: 'name_ASC',
-        where: {
-          AND: [
-            { isConfirmed: true },
-            { major: { name: major } },
-            { graduatedYear: { generation: generation } },
-          ],
-        },
-      });
+      const users = await prisma
+        .users({
+          orderBy: 'name_ASC',
+          where: {
+            AND: [
+              { isConfirmed: true },
+              { major: { name: major } },
+              { graduatedYear: { generation } },
+            ],
+          },
+        })
+        .$fragment(FRAGMENT);
 
       return users
-        .sort(compareByName)
+        .sort(compareByGenName)
         .slice((page - 1) * limit, (page - 1) * limit + limit);
     },
   },
